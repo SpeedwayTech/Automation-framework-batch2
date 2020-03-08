2bbb314d6f3aec0ad4991b1ff104717b85a0a9c0
Ganesh <ganeshkakade@congizant.com>
2020-03-09 03:27:47 +0530

    just comment added

--- src/test/java/com/crm/qa/testcases/LoginPageTest.java
+++ src/test/java/com/crm/qa/testcases/LoginPageTest.java
@@ -1,5 +1,7 @@
 package com.crm.qa.testcases;
-
+/*
+ * cloned project
+ */
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
