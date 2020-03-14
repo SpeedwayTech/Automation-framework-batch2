79b29435eabac030c6b77ee58fd17781abd38698
jordan <janardhan.karagir@gmail.com>
2020-03-14 22:37:57 +0530

    Added LoginTest

--- src/test/java/com/crm/qa/testcases/LoginPageTest.java
+++ src/test/java/com/crm/qa/testcases/LoginPageTest.java
@@ -1,28 +1,29 @@
-/*package com.crm.qa.testcases;
+package com.crm.qa.testcases;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.crm.qa.base.TestBase;
-import com.crm.qa.pages.HomePage;
 import com.crm.qa.pages.LoginPage;
 import com.crm.qa.util.RetryCountIfFailed;
 
 public class LoginPageTest extends TestBase{
 	LoginPage loginPage;
-	HomePage homePage;
+	
 	
 	@BeforeMethod
 	public void setUp(){
 		initialization();
-		loginPage = new LoginPage();	
+		loginPage = new LoginPage(driver);	
 	}
 	
 	@Test(priority=0)
 	@RetryCountIfFailed(2)
 	public void loginTest(){
-		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+		loginPage.enterUsername(prop.getProperty("username"));
+		loginPage.enterPassword(prop.getProperty("password"));
+		loginPage.clickonLoginBtn();
 	}
 		
 	@AfterMethod
@@ -35,4 +36,3 @@ public class LoginPageTest extends TestBase{
 	
 
 }
-*/
